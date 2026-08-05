74e86c2f7f86171523814063eacd2b395db4c9f7
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-06 00:52:12 +0530

    Add operator function

---
+++ src/Java_Practice/Operators.java
@@ -0,0 +1,10 @@
+package Java_Practice;
+public class Operators {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println("Sum of Two numbes is: "+sum);
+    }
+}
+//Same for other Operators "- , / , % , *"
