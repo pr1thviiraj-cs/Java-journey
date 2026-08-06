91f983fe2441eba1c4392938dd57bf24df15921e
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-06 10:41:28 +0530

    Add Simple Intrest Function

--- src/Java_Practice/Operators.java
+++ src/Java_Practice/Operators.java
@@ -5,10 +5,18 @@ public class Operators {
        int breadth = 10;
        int area = length * breadth;
         System.out.println("The Area of Rectangle is: "+area);
-
+//        Area Of Circle...
         float radius = 7.0F;
         float Area = 3.14F * radius * radius;
         System.out.println("The Area of Rectangle is: "+Area);
+
+//        Simple Intrest Function..
+
+        int P = 750000;
+        float R = 9.5F;
+        int T = 14;
+        float SI = (P*R*T)/100;
+        System.out.println("The Simple Intrest is: "+SI);
     }
 }
 
