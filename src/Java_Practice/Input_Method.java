71270917291633b988bd6056dabbd58748ad8582
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-06 11:02:30 +0530

    Sum of two number via input methods

--- src/Java_Practice/Input_Method.java
+++ src/Java_Practice/Input_Method.java
@@ -7,5 +7,17 @@ public class Input_Method{
         System.out.println("Enetr Your Name: ");
         name = sc.nextLine();
         System.out.println("Your Name is: "+name);
+
+//        Taking input of two Numbers and printing their sum..
+
+        int a;
+        System.out.println("Enter Your Frist Number: ");
+        a = sc.nextInt();
+        int b;
+        System.out.println("Enter Your Second Number: ");
+        b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("The Sum of Both the numbers is: "+sum);
+
     }
 }
