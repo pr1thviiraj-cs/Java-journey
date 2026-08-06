6c98e364ac0ba251bf84f0128e10b0e2a2a7072c
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-06 10:21:54 +0530

    Delete old variable and data types and add new

---
+++ src/Java_Practice/Variables_Data_Types.java
@@ -0,0 +1,14 @@
+package Java_Practice;
+
+import javax.xml.namespace.QName;
+
+public class Variables_Data_Types {
+    public static void main(String[] args){
+        String Name = "Prithviraj";
+        int Age = 19;
+        float Percentage = 87.5F;
+        System.out.println("Name: "+ Name);
+        System.out.println("Age is: "+Age);
+        System.out.println("Perentage is: "+Percentage);
+    }
+}
