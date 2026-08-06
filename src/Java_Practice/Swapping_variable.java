a28fe79278ddd4d3ac501df9349f55e48808c5f4
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-06 10:25:48 +0530

    Swappiing of Variables using temp

---
+++ src/Java_Practice/Swapping_variable.java
@@ -0,0 +1,12 @@
+package Java_Practice;
+public class Swapping_variable {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("Value of A is: "+a);
+        System.out.println("Value of B is: "+b);
+    }
+}
