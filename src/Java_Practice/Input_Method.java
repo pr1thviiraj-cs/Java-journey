a3378804155122ef89bfa7be8ffc55140d3da475
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-06 10:58:22 +0530

    Scanner Method

---
+++ src/Java_Practice/Input_Method.java
@@ -0,0 +1,11 @@
+package Java_Practice;
+import java.util.*;
+public class Input_Method{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("Enetr Your Name: ");
+        name = sc.nextLine();
+        System.out.println("Your Name is: "+name);
+    }
+}
