46b00fa3761494ac0bf35f2bebd3176d34d9d42f
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-06 10:31:17 +0530

    Swapping Without temp variable

---
+++ src/Java_Practice/Swapp_NOV.java
@@ -0,0 +1,12 @@
+package Java_Practice;
+public class Swapp_NOV {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("The Value of A is: "+ a);
+        System.out.println("The Value of B is: "+ b);
+    }
+}
