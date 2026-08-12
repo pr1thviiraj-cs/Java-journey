e6dbe64618d2b7d9e170daca6d3e20dc6c7f9ddc
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-12 23:39:41 +0530

    Add java methods v2

--- src/Java_Practice/Methods.java
+++ src/Java_Practice/Methods.java
@@ -4,7 +4,11 @@ public class Methods {
     public static void printjava(){
         System.out.println("Hello Prithviraj");
     }
+    public static void printname(String name){
+        System.out.println(name);
+    }
     public static void main(String[] args){
         printjava();
+        printname("Prithviraj");
     }
 }
