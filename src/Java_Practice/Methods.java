d9cafe7ef946fdb5809e48a6862581b6340daeda
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-12 23:32:24 +0530

    Add java methods

---
+++ src/Java_Practice/Methods.java
@@ -0,0 +1,10 @@
+package Java_Practice;
+import java.util.Scanner;
+public class Methods {
+    public static void printjava(){
+        System.out.println("Hello Prithviraj");
+    }
+    public static void main(String[] args){
+        printjava();
+    }
+}
