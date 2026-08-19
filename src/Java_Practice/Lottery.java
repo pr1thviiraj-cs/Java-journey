b9134786dce687c752ac752907bcba9f49967b09
Prithviraj Singh Rathore <prithviiraj.cs@gmail.com>
2026-08-19 23:15:46 +0530

    Add Lottery Programe

---
+++ src/Java_Practice/Lottery.java
@@ -0,0 +1,58 @@
+package Java_Practice;
+import java. util.Scanner;
+public class Lottery {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Generate lottery number between 10 and 99
+        int lottery = (int)(Math.random() * 90) + 10;
+
+        System.out.print("Enter a two-digit number: ");
+        int user = sc.nextInt();
+        // Validation of 2- digit number
+        if (user < 10 || user > 99) {
+            System.out.println("Invalid Input!");
+            System.out.println("Please enter a TWO-DIGIT number.");
+            sc.close();
+            return;
+        }
+
+        int lotteryDigit1 = lottery / 10;
+        int lotteryDigit2 = lottery % 10;
+
+        int userDigit1 = user / 10;
+        int userDigit2 = user % 10;
+
+        System.out.println("Lottery Number = " + lottery);
+
+        // Exact match
+        if (user == lottery) {
+            System.out.println("🎉 Exact Match!");
+            System.out.println("You won $10,000!");
+
+            // Same digits, different order
+        } else if (userDigit1 == lotteryDigit2 &&
+                userDigit2 == lotteryDigit1) {
+
+            System.out.println("🎉 Both digits match!");
+            System.out.println("You won $3,000!");
+
+            // One digit matches
+        } else if (userDigit1 == lotteryDigit1 ||
+                userDigit1 == lotteryDigit2 ||
+                userDigit2 == lotteryDigit1 ||
+                userDigit2 == lotteryDigit2) {
+
+            System.out.println("🎉 One digit matches!");
+            System.out.println("You won $1,000!");
+
+        } else {
+            System.out.println("No match.");
+            System.out.println("You won $0.");
+        }
+
+        sc.close();
+    }
+}
+
